442d8605c79a48531005cb5c5292b1a702e1db4b
boguwyrw <b.wyrwal@wp.pl>
2020-10-08 21:31:21 +0200

    Characters concept was changed

---
+++ CreepyHouse/Assets/Scripts/CharactersScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CharactersScript : MonoBehaviour
+{
+    [SerializeField]
+    private Button startGameButton;
+    [SerializeField]
+    private Toggle cleverGuyToggle;
+    [SerializeField]
+    private Toggle strongGuyToggle;
+
+    private void Start()
+    {
+        startGameButton.interactable = false;
+    }
+
+    private void Update()
+    {
+        if (cleverGuyToggle.isOn == false && strongGuyToggle.isOn == false)
+        {
+            startGameButton.interactable = false;
+        }
+        else
+        {
+            startGameButton.interactable = true;
+        }
+    }
+
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ CreepyHouse/Assets/Scripts/GuyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuyScript : MonoBehaviour
+{
+    [SerializeField]
+    private NewCharacterScript newCharacter;
+
+    [SerializeField]
+    private Text characterNameText;
+    [SerializeField]
+    private Image characterImage;
+    [SerializeField]
+    private Text healthValueText;
+    [SerializeField]
+    private Text strengthValueText;
+    [SerializeField]
+    private Text dexterityValueText;
+    [SerializeField]
+    private Text staminaValueText;
+    [SerializeField]
+    private Text artificeValueText;
+
+    private void Start()
+    {
+        characterNameText.text = newCharacter.characterName;
+        characterImage.sprite = newCharacter.characterImage;
+        healthValueText.text = newCharacter.health.ToString();
+        strengthValueText.text = newCharacter.strength.ToString();
+        dexterityValueText.text = newCharacter.dexterity.ToString();
+        staminaValueText.text = newCharacter.stamina.ToString();
+        artificeValueText.text = newCharacter.artifice.ToString();
+    }
+}
