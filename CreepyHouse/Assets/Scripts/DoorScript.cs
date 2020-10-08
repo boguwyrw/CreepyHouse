b04ef7816a55d31c947b16121ce28ed5070e4ba5
boguwyrw <b.wyrwal@wp.pl>
2020-10-09 00:30:07 +0200

    Building the game scene - door system

---
+++ CreepyHouse/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private float doorWidth = 0.0f;
+    private Vector3 rotationVector;
+
+    void Start()
+    {
+        doorWidth = gameObject.GetComponent<BoxCollider>().size.x;
+        rotationVector = new Vector3(transform.position.x + 1.5f, transform.position.y, transform.position.z);
+    }
+
+    void Update()
+    {
+        if (transform.localEulerAngles.y < 90.0f)
+        {
+            transform.RotateAround(rotationVector, Vector3.up, 20 * Time.deltaTime);
+        }
+    }
+}
