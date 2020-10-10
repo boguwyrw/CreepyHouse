d830d124389ddfffcdfec97b80189cd3b8fa0b8b
boguwyrw <b.wyrwal@wp.pl>
2020-10-10 14:46:11 +0200

    Door system was updated

--- CreepyHouse/Assets/Scripts/DoorScript.cs
+++ CreepyHouse/Assets/Scripts/DoorScript.cs
@@ -1,26 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorScript : MonoBehaviour
 {
+    [SerializeField]
+    private Button actionButton;
+
     private float doorWidth = 0.0f;
     private Vector3 rotationVector;
+    private float doorOpeningSpeed = 17.5f;
+    private bool madeAction = false;
+    private bool doorAreOpen = false;
 
-    void Start()
+    private void Start()
     {
-        doorWidth = gameObject.GetComponent<BoxCollider>().size.x;
-        rotationVector = new Vector3(transform.position.x + 1.0f, transform.position.y, transform.position.z);
-        transform.RotateAround(rotationVector, Vector3.up, 15.0f);
+        doorWidth = gameObject.GetComponent<Renderer>().bounds.size.x;
+        rotationVector = new Vector3(transform.position.x + doorWidth/2, transform.position.y, transform.position.z);
     }
 
-    void Update()
+    private void Update()
     {
-        /*
-        if (transform.localEulerAngles.y < 90.0f)
+        if (!doorAreOpen && madeAction)
         {
-            transform.RotateAround(rotationVector, Vector3.up, 20 * Time.deltaTime);
+            transform.RotateAround(rotationVector, Vector3.up, doorOpeningSpeed * Time.deltaTime);
+            if (transform.localEulerAngles.y >= 90.0f)
+            {
+                madeAction = false;
+                doorAreOpen = true;
+            }
         }
-        */
+
+        if (doorAreOpen && madeAction)
+        {
+            transform.RotateAround(rotationVector, Vector3.down, doorOpeningSpeed * Time.deltaTime);
+            if (transform.localEulerAngles.y <= 1.0f)
+            {
+                madeAction = false;
+                doorAreOpen = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            actionButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            actionButton.gameObject.SetActive(false);
+        }
+    }
+    
+    public void ActionButton()
+    {
+        madeAction = true;
     }
 }
