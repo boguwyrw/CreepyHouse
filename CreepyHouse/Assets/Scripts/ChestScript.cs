f62ba3b3d5ff109269351ac7eb30c8871c3b1443
boguwyrw <b.wyrwal@wp.pl>
2020-10-12 18:43:32 +0200

    Chest system - in progress

--- CreepyHouse/Assets/Scripts/ChestScript.cs
+++ CreepyHouse/Assets/Scripts/ChestScript.cs
@@ -1,18 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Button wreckingBarButton;
+    [SerializeField]
+    private Button axButton;
+    [SerializeField]
+    private Text chestInfoText;
+    [SerializeField]
+    private GameObject playerEquipment;
+    [SerializeField]
+    private GameObject player;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.layer == 9)
+        {
+            wreckingBarButton.gameObject.SetActive(true);
+            axButton.gameObject.SetActive(true);
+            CheckPlayerEquipment();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.gameObject.layer == 9)
+        {
+            wreckingBarButton.gameObject.SetActive(false);
+            axButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void CheckPlayerEquipment()
+    {
+        if (playerEquipment.transform.childCount > 0)
+        {
+            for (int i = 0; i < playerEquipment.transform.childCount; i++)
+            {
+                string nameOfItemInInventory = playerEquipment.transform.GetChild(i).name;
+                if (nameOfItemInInventory.Equals("WreckingBar"))
+                {
+                    wreckingBarButton.interactable = true;
+                    axButton.interactable = false;
+                }
+                else if (nameOfItemInInventory.Equals("Ax"))
+                {
+                    wreckingBarButton.interactable = false;
+                    axButton.interactable = true;
+                }
+            }
+        }
+        else
+        {
+            wreckingBarButton.interactable = false;
+            axButton.interactable = false;
+        }
     }
 }
