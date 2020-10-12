b98ea92695ca583fbcc07755bb5f273cfd6b690b
boguwyrw <b.wyrwal@wp.pl>
2020-10-13 00:06:53 +0200

    Exit door system in progress
    Tasks
    1. Finish exit door system
    2. Make end game
    3. Make roof for the house

--- CreepyHouse/Assets/Scripts/ExitDoorScript.cs
+++ CreepyHouse/Assets/Scripts/ExitDoorScript.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitDoorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Button openExitDoorButton;
+    [SerializeField]
+    private Text exitDoorInfoText;
+    [SerializeField]
+    private GameObject playerEquipment;
+
+    private float doorWidth = 0.0f;
+    private Vector3 rotationVector;
+    private float doorOpeningSpeed = 50.0f;
+
+    private void Start()
     {
-        
+        doorWidth = gameObject.GetComponent<Renderer>().bounds.size.x;
+        rotationVector = new Vector3(transform.position.x + doorWidth / 2, transform.position.y, transform.position.z);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckPlayerEquipment()
     {
-        
+        if (playerEquipment.transform.childCount > 0)
+        {
+            for (int i = 0; i < playerEquipment.transform.childCount; i++)
+            {
+                string nameOfItemInInventory = playerEquipment.transform.GetChild(i).name;
+                if (nameOfItemInInventory.Equals("Key"))
+                {
+                    openExitDoorButton.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            CheckPlayerEquipment();
+        }
+    }
+
+    public void OpenExitDoorButton()
+    {
+        transform.RotateAround(rotationVector, Vector3.up, doorOpeningSpeed * Time.deltaTime);
     }
 }
