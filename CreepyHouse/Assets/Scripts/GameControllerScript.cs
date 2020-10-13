7bb14c955c50b6aa1ab651e538b680f55138e64d
boguwyrw <b.wyrwal@wp.pl>
2020-10-13 13:34:36 +0200

    End game system - in progress
    Tasks
    1. Roof for the house
    2. Add end game image

---
+++ CreepyHouse/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControllerScript : MonoBehaviour
+{
+    [SerializeField]
+    private Text endGameInfoText;
+
+    private void Update()
+    {
+        if (PlayerGuyScript.playerHealth == 0)
+        {
+            Time.timeScale = 0.0f;
+            endGameInfoText.color = Color.red;
+            endGameInfoText.text = "SORRY, YOU LOST";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            Time.timeScale = 0.0f;
+            endGameInfoText.text = "CONGRATULATION, YOU WIN";
+            endGameInfoText.gameObject.SetActive(true);
+        }
+    }
+}
